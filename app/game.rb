1bc747848997caedd4584ee82af40fc176f98ac2
Will Sams <willsams@hotmail.com>
2018-02-19 18:03:29 -0500

    Doneski

--- app/game.rb
+++ app/game.rb
@@ -1,13 +1,51 @@
 class Game
   def initialize
-    @total_score = 0
+    @rolls = Array.new(21, 0)
+    @current_roll = 0
   end
 
   def roll(pins)
-     @total_score += pins
+    @rolls[@current_roll] = pins
+    @current_roll += 1
   end
 
   def score
-    @total_score
+    total_score = 0
+    frame_index = 0
+
+    10.times do  #10 frames
+      if strike?(frame_index)
+        total_score += strike_bonus(frame_index)
+        frame_index += 1
+      elsif spare?(frame_index)
+        total_score += spare_bonus(frame_index)
+        frame_index +=2
+      else
+        total_score += sum_of_pins_down_in_frame(frame_index)
+        frame_index += 2
+      end
+    end
+    total_score
   end
+
+  private
+    def spare?(frame_index)
+      @rolls[frame_index] + @rolls[frame_index + 1] == 10
+    end
+
+    def strike?(frame_index)
+      @rolls[frame_index] == 10
+    end
+
+    def spare_bonus(frame_index)
+      10 + @rolls[frame_index + 2]
+    end
+
+    def strike_bonus(frame_index)
+      10 + @rolls[frame_index+1] + @rolls[frame_index+2]
+    end
+
+    def sum_of_pins_down_in_frame(frame_index)
+      @rolls[frame_index] + @rolls[frame_index + 1]
+    end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -26,10 +26,46 @@ describe Game do
 
     it "bowler only rolled a spare plus 3" do
       @game.roll(5)
-      @game.roll(5)   #spare strike!
+      @game.roll(5)               #spare strike!
       @game.roll(3)
       17.times { @game.roll(0)}   #other 17 rolls were duds
-      expect(@game.score).to 13
+      expect(@game.score).to equal 16
+    end
+
+    it "bowler only rolled a spare plus 3 in a middle frame" do
+      8.times { @game.roll(0)}    #bowler start the game on a 10 roll gutter streak!
+      @game.roll(5)
+      @game.roll(5)               #spare strike!
+      @game.roll(3)
+      9.times { @game.roll(0)}   #bowler ended game much like how he/she started....terri-bad
+      expect(@game.score).to equal 16
+    end
+
+    it "bowlers rolled a strike, then x and xx" do
+      @game.roll(10)
+      @game.roll(3)
+      @game.roll(4)
+      17.times { @game.roll(0)}
+      expect(@game.score).to equal 24
+    end
+
+    it "bowlers rolled a strike, then x and xx in middle frames" do
+      8.times { @game.roll(0)}    #bowler start the game on a 10 roll gutter streak!
+      @game.roll(10)
+      @game.roll(3)
+      @game.roll(4)
+      9.times { @game.roll(0)}   #bowler ended game much like how he/she started....terri-bad
+      expect(@game.score).to equal 24
+    end
+
+    it "bowler rolled a perfect game" do
+      20.times { @game.roll(10)}
+      expect(@game.score).to equal 300
+    end
+
+    it "bowler rolled a perfect game" do
+      20.times { @game.roll(10)}
+      expect(@game.score).to equal 300
     end
   end
 end
