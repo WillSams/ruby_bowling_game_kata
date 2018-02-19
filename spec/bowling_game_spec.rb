4b0e40348fdcf5f04abe8803d26e7182fabd809b
Will Sams <willsams@hotmail.com>
2018-02-19 08:50:03 -0500

    Added references

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -18,10 +18,18 @@ describe Game do
       20.times { @game.roll(0) }
       expect(@game.score).to equal 0
     end
-    
+
     it "bowler knocked down 1 pin each roll" do
       20.times { @game.roll(1) }
       expect(@game.score).to equal 20
+    end
+
+    it "bowler only rolled a spare plus 3" do
+      @game.roll(5)
+      @game.roll(5)   #spare strike!
+      @game.roll(3)
+      17.times { @game.roll(0)}   #other 17 rolls were duds
+      expect(@game.score).to 13
     end
   end
 end
